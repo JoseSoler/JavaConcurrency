75c24e6cef8e9355c1c977f144897dd3ff37f988
Jose Soler <josocas@gmail.com>
2015-05-17 20:11:05 +0200

    Add first two exercises

---
+++ src/main/java/es/josocas/ConcurrentSender.java
@@ -0,0 +1,39 @@
+package es.josocas;
+
+/**
+ * Created by jsoler on 17.05.15.
+ */
+public class ConcurrentSender {
+
+    public static void main(String[] args){
+
+        Runnable sendingWork = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Sending DHL Packet... such a heavy one :..-(");
+                try {
+                   Thread.currentThread().sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+
+        Thread t1 = new Thread(sendingWork);
+        t1.run();
+
+        //Doing very important stuff cannot wait meanwhile...
+
+        //Now let's check if the worker has finished
+        while (t1.getState() != Thread.State.TERMINATED){
+            System.out.println("Waiting for the sending work to finish...");
+            try {
+                Thread.currentThread().sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
---
+++ src/main/java/es/josocas/CyclingRace.java
@@ -0,0 +1,41 @@
+package es.josocas;
+
+/**
+ * Created by jsoler on 17.05.15.
+ */
+public class CyclingRace {
+
+    public static void main(String[] args) {
+
+        Runnable giroItalia = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + " says:  Starting the Race....");
+
+                for (int km = 0; km < 3000; km++) {
+                    try {
+                        Thread.currentThread().sleep(10); //I'm sorry guys, you can only sleep 10 millis..
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                System.out.println(Thread.currentThread().getName() + " says:  I've just finished the Race, Mama mia !!");
+            }
+        };
+
+
+        Thread contador = new Thread(giroItalia);
+        Thread aru = new Thread(giroItalia);
+        Thread porte = new Thread(giroItalia);
+
+        contador.setName("Alberto Contador");
+        aru.setName("Fabio Aru");
+        porte.setName("Richie Porte");
+
+        contador.start();
+        aru.start();
+        porte.start();
+
+    }
+}
